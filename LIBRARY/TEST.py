754faeddde88e49ac03d5386f513c124a472e270
Carollllk <117477375+Carollllk@users.noreply.github.com>
2022-11-05 18:41:48 -0400

    Add files via upload

---
+++ LIBRARY/TEST.py
@@ -0,0 +1,14 @@
+import unittest
+import math
+from main import Main
+
+
+class dataTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.main = Main(2)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
